c7d607997787ab0cbca1343d575c858e26a9530a
LaLoca1 <alex.olalla13@gmail.com>
2020-11-19 18:55:35 +0000

    Add files via upload

---
+++ userfeed/views.py
@@ -0,0 +1,46 @@
+from django.conf import settings
+from django.shortcuts import render
+
+from rest_framework.decorators import api_view,permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from .models import Tweet
+from .serializers import TweetSerializer
+
+ALLOWED_HOSTS = settings.ALLOWED_HOSTS
+
+# Create your views here.
+def home_view(request, *args, **kwargs):
+    username = None
+    if request.user.is_authenticated:
+        username = request.user.username
+    return render(request, "pages/home.html", context={"username": username}, status=200)
+
+
+@api_view(['POST'])  # http method the client == POST
+@permission_classes([IsAuthenticated])  # REST API course
+def tweet_create_view(request, *args, **kwargs):
+    serializer = TweetSerializer(data=request.POST)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save(user=request.user)
+        return Response(serializer.data, status=201)
+    return Response({}, status=400)
+
+
+@api_view(['GET'])
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    qs = Tweet.objects.filter(id=tweet_id)
+    if not qs.exists():
+        return Response({}, status=404)
+    #obj = qs.first()
+    serializer = TweetSerializer(qs, many=False)
+    return Response(serializer.data, status=200)
+
+
+@api_view(['GET'])
+def tweet_list_view(request, *args, **kwargs):
+    qs = Tweet.objects.all()
+    serializer = TweetSerializer(qs, many=True)
+    return Response(serializer.data, status=200)
+
+
